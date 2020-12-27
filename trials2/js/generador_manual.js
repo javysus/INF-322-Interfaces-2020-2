e9c8560e5b524a340c1b5e18a622e2a2675ca524
Javiera Villarroel <jvillarroeltoloza@gmail.com>
2020-12-27 18:32:59 -0300

    Avance generador manual: Botones y eliminar

--- trials2/js/generador_manual.js
+++ trials2/js/generador_manual.js
@@ -8,16 +8,33 @@ $(document).ready(function(){
         var genero = document.getElementById("genero").value;
         var nivel = document.getElementById("nivel").value;
         var tabla = document.getElementById("tableBody");
+        var idAlumno = alumno;
 
         console.log(tabla);
 
-        $("#tableBody").append('<tr><th scope="row">'
-        +alumno+'</th><td>'+nombre+'</td><td>'+apellido+'</td><td>'+genero+'</td><td>'+nivel+'</td></tr>');
+        $("#tableBody").append('<tr id=dato'+alumno+'><th scope="row" id=n'+alumno+'>'
+        +alumno+'</th><td>'+nombre+'</td><td>'+apellido+'</td><td>'+genero+'</td><td>'+nivel+'</td><td><button id='+
+        alumno+' type="button" class="btn btn-danger btn-sm">X</button></td></tr>');
 
         alumno++;
 
         document.getElementById("nombre").value = "";
         document.getElementById("apellido").value = "";
-    })
 
+        $('#'+idAlumno).click(function(){
+
+            document.getElementById('dato'+idAlumno).innerHTML = "";
+    
+            console.log('id alumno'+idAlumno);
+            console.log(alumno);
+            var i = idAlumno+1;
+            console.log(i);
+            for(; i < alumno; i++){
+                console.log("Hola "+i);
+                document.getElementById("n"+i).innerHTML = i-1;
+            }
+        })
+    
+    })
+    
 })
