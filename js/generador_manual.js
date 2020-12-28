c36801068bb57a50c17db247816ebacae8071a33
Jorge Salas <jorge.salas@alumnos.inf.utfsm.cl>
2020-12-28 16:28:03 -0300

    Restauración commit previo

--- js/generador_manual.js
+++ js/generador_manual.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
+    var alumnosv = 1;
     var alumno = 1;
 
     $('#add').click(function(){
@@ -8,16 +9,32 @@ $(document).ready(function(){
         var genero = document.getElementById("genero").value;
         var nivel = document.getElementById("nivel").value;
         var tabla = document.getElementById("tableBody");
+        var idAlumno = alumno;
 
         console.log(tabla);
 
-        $("#tableBody").append('<tr><th scope="row">'
-        +alumno+'</th><td>'+nombre+'</td><td>'+apellido+'</td><td>'+genero+'</td><td>'+nivel+'</td></tr>');
+        $("#tableBody").append('<tr id=dato'+alumno+'><th scope="row" id=n'+alumno+'>'
+        +alumnos+'</th><td>'+nombre+'</td><td>'+apellido+'</td><td>'+genero+'</td><td>'+nivel+'</td><td><button id='+
+        alumno+' type="button" class="btn btn-danger btn-sm">X</button></td></tr>');
 
         alumno++;
+        alumnos++;
 
         document.getElementById("nombre").value = "";
         document.getElementById("apellido").value = "";
-    })
 
+        $('#'+idAlumno).click(function(){
+
+            document.getElementById('dato'+idAlumno).innerHTML = "";
+            alumnos--;
+            
+            var i = idAlumno+1;
+            for(; i < alumno; i++){
+                document.getElementById("n"+i).innerHTML = parseInt(document.getElementById("n"+i).innerHTML)-1;
+            }
+            
+        })
+    
+    })
+    
 })
